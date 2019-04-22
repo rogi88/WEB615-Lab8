968f7ac191f45ce5ce71fb56c3e92d269eb7e8d0
Rogi88 <igor.peric777@gmail.com>
2019-04-22 11:46:42 -0400

    added users_spec.rb

--- Gemfile
+++ Gemfile
@@ -82,9 +82,9 @@ end
 group :production do
   gem 'pg', '~> 0.21'
   gem 'rails_12factor'
+  ruby '2.6.1'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-ruby '2.4.1'
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :article do
-    sequence(:title) { |n| "title_#{n}" } # This allows for many articles to be created at the same time
-    content "This is my content"
-    user # This requires that a user model to be created (or specified) and associated with the record
+    
   end
 end
